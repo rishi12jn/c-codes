864b45e299e520396ff77ce42407921a2e607f42
Rishi Raj Jain <141619921+rishi12jn@users.noreply.github.com>
2024-05-27 22:19:57 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool ispresent(int arr[][4],int target,int i,int j){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            if(arr[i][j]==target){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+   
+   //create 2d array
+    int arr[3][4];
+
+    // taking input for array
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cin>>arr[i][j];
+        }
+    }
+   //print array
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+
+        cout<<"enter the element to search"<<endl;
+        int target;
+        cin>>target;
+        if(ispresent(arr,target,3,4)){
+            cout<<"element not found"<<endl;
+        }
+        else{
+            cout<<"not present"<<endl;
+        }
+    }
+}
